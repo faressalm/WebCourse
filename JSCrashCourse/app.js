5972d3c4594a944d32b3371c0d1426438103c91f
Fares Waheed <faressalm44@hotmail.com>
2021-09-08 18:52:21 +0200

    intialize objects instances

--- JSCrashCourse/app.js
+++ JSCrashCourse/app.js
@@ -1,6 +1,24 @@
-
-for (let i = 1; i <= 6; i++) {
-    document.write( "<h"+i+ ">) fares waheed <br>");
-    // i can write anything that could be writen on html 
+// create objects
+//1
+person={name:  "ahmed",
+        number:"154322",
+        class: "csed" };
+ //2
+ var cat= new Object();
+ cat.name="bo2somat";
+ cat.color="white";
+ cat.age=1;
+ document.write(cat.name+cat.age+"<br>");
+ //3
+ function catconstructor(age,name,color){
+     this.age=age;
+     this.name=name;
+     this.color=color;
+ }
+ emp =new catconstructor(1,"bo2somat","white");  
+ document.write(emp.name+emp.age);     
+/*for (let i = 1; i <= 6; i++) {
     
-}
+    document.write( "<h"+person.number[i-1]+ "> fares waheed <br>");
+    // i can write anything that could be writen on html 
+}*/
