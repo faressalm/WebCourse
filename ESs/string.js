7e06decf4a59abb3d69c51e28d027de32638a382
Fares Waheed <faressalm44@hotmail.com>
2021-10-07 01:03:42 +0200

    add string

---
+++ ESs/string.js
@@ -0,0 +1,13 @@
+var myName = 'Muhammed Essa'
+// to check if string or character is included in other string start at specific index
+myName.startsWith('Gh',0) 
+
+//to check if string or character is included in other string ends at specific index
+myName.endsWith('an',7) 
+
+//to check if string or character is included in other string start from index i till end of string
+console.log(  myName.includes('Essa', 5) );
+
+// to repear string 
+console.log(  "_".repeat(30) )
+
